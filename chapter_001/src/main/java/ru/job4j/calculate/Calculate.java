fc984871885755b3cbbac56c0e04780c27fd5014
Viktor Kusliy <viktor.kusliy@gmail.com>
2018-09-21 20:40:18 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package main.java.ru.job4j.calculate;
-
+/**
+* Calculate.
+*
+*@author Viktor Kusliy (viktor.kusliy@gmail.com)
+*/
 public class Calculate{
+	
+	/**
+	* Main.
+	*@param args - args.
+	*/
 	
 	public static void main(String[] args){
 	System .out.println("Hello World");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Viktor Kusliy (mailto:viktor.kusliy@gmail.com)
+* @version 1.0
+* @since 21.09.2018
+*/
+package ru.job4j.calculate;
