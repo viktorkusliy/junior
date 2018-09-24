60241d20ac4a61ac870118e93c242b42d2d6465f
Viktor Kusliy <viktor.kusliy@gmail.com>
2018-09-24 19:32:05 +0300

    1.4. JUnit [#4122] corrected version

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Viktor Kusliy (mailto:viktor.kusliy@gmail.com)
+* @version 1.0
+* @since 24.09.2018
+*/
+
+public class CalculateTest {
+/**
+* Test echo.
+*/
+
+	@Test
+	public void whenTakeNameThenTreeEchoPlusName(){
+		String input = "Viktor Kusliy";
+		String expect = "Echo, echo, echo : Viktor Kusliy";
+		Calculate calculate = new Calculate();
+		String result = calculate.echo(input);
+		assertThat(result, is(expect));
+		
+	}	
+	
+	
+	
+	
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate test.
+*
+* @author Viktor Kusliy (mailto:viktor.kusliy@gmail.com)
+* @version 1.0
+* @since 24.09.2018
+*/
+package ru.job4j.calculate;
