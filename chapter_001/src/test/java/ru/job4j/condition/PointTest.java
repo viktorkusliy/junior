af94b04094139167f5d98c67b71819e9175449ce
Viktor Kusliy <viktor.kusliy@gmail.com>
2018-09-28 13:57:01 +0300

    Class for calculating the distance between two points.

---
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+
+public class PointTest {
+    @Test
+    public void calculationOfTheDistanceBetweenTwoPoints() {
+        Point one = new Point(1, 1);
+        Point two = new Point(2, 2);
+        assertThat(one.distanceTo(two), closeTo(1.41, 0.1));
+    }
+
+}
