5adbaa0a1cd1c3a2f1e9ccce4e3b3d80a0130dde
Viktor Kusliy <viktor.kusliy@gmail.com>
2018-10-01 21:58:44 +0300

    creating a not very clever bot

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,29 @@
+package ru.job4j.condition;
+
+/**
+ * Not quite smart bot.
+ * @author Viktor Kusliy (mailto:viktor.kusliy@gmail.com).
+ * @version 1.0.
+ * @since 01.10.2018.
+ */
+
+public class DummyBot {
+
+    /**
+     * A method for bot answering questions.
+     * @param question Question from the client.
+     * @return Reply from bot.
+     */
+    public String answer(String question) {
+        String answer = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if (question.equals("Привет, Бот.")) {
+            answer = "Привет, умник.";
+        } else if (question.equals("Пока.")) {
+            answer = "До скорой встречи.";
+        }
+        return answer;
+    }
+
+
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Testing is not very smart bot.
+ * @author Viktor Kusliy (mailto:viktor.kusliy@gmail.com).
+ * @version 1.0.
+ * @since 01.10.2018.
+ */
+public class DummyBotTest {
+
+    @Test
+    public void whenGreetBot() {
+        DummyBot dummyBot = new DummyBot();
+        assertThat(dummyBot.answer("Привет, Бот."), is("Привет, умник."));
+    }
+
+    @Test
+    public void whenByeBot() {
+        DummyBot dummyBot = new DummyBot();
+        assertThat(dummyBot.answer("Пока."), is("До скорой встречи."));
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        DummyBot dummyBot = new DummyBot();
+        assertThat(dummyBot.answer("Сколько будет 2 + 2 ?"), is("Это ставит меня в тупик. Спросите другой вопрос."));
+    }
+
+
+}
