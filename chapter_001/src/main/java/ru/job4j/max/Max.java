2eb35272b2520152035ff298aa2efa9ed08b239f
Viktor Kusliy <viktor.kusliy@gmail.com>
2018-10-03 22:27:17 +0300

    adding a method to find the maximum of three numbers v.2

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -3,7 +3,7 @@ package ru.job4j.max;
 /**
  * Finding the maximum of two numbers.
  * @author Viktor Kusliy (mailto:viktor.kusliy@gmail.com).
- * @version 1.0.
+ * @version 2.0.
  * @since 03.10.2018.
  */
 public class Max {
@@ -16,6 +16,19 @@ public class Max {
      */
     public int max(int first, int second) {
         return first > second ? first : second;
+    }
+
+    /**
+     * Method to find the maximum of three numbers.
+     * @param first First number.
+     * @param second Second number.
+     * @param third Third number.
+     * @return Maximum three numbers.
+     */
+    public int max(int first, int second, int third) {
+        int temp = this.max(first, second);
+        temp = this.max(temp, third);
+        return temp;
     }
 
 
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -5,9 +5,9 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
- * The test class finding the maximum number of two numbers.
+ * The test class finding the maximum number of three numbers.
  * @author Viktor Kusliy (mailto:viktor.kusliy@gmail.com).
- * @version 1.0.
+ * @version 2.0.
  * @since 03.10.2018.
  */
 public class MaxTest {
@@ -24,6 +24,27 @@ public class MaxTest {
         Max maximum = new Max();
         int result = maximum.max(2, 1);
         assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenFirstIsMoreThanSecondAndThird() {
+        Max maximum = new Max();
+        int result = maximum.max(3, 2, 1);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenSecondIsMoreThanFirstAndThird() {
+        Max maximum = new Max();
+        int result = maximum.max(1, 3, 2);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenThirdIsMoreThanFirstAndSecond() {
+        Max maximum = new Max();
+        int result = maximum.max(1, 2, 3);
+        assertThat(result, is(3));
     }
 
 
