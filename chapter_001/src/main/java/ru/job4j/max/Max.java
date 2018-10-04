f084c6c2bac71574823bd9986bbc347a4481c5dd
Viktor Kusliy <viktor.kusliy@gmail.com>
2018-10-04 09:04:12 +0300

    method editing to find the maximum of three numbers

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -27,8 +27,7 @@ public class Max {
      */
     public int max(int first, int second, int third) {
         int temp = this.max(first, second);
-        temp = this.max(temp, third);
-        return temp;
+        return this.max(temp, third);
     }
 
 
