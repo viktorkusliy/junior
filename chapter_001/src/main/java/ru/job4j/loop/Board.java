286695499dff42b404c69a836fb855cb95992de2
Viktor Kusliy <viktor.kusliy@gmail.com>
2018-10-08 08:39:18 +0300

    building a chessboard of a given width and height

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+/**
+ * Construction of a chessboard.
+ * @author Viktor Kusliy (mailto:viktor.kusliy@gmail.com).
+ * @version 1.0.
+ * @since 08.10.2018.
+ */
+public class Board {
+
+    /**
+     * Method for building a chessboard of a given width and height.
+     * @param width Width chessboard.
+     * @param height Height chessboard.
+     * @return Chessboard.
+     */
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String line = System.lineSeparator();
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++) {
+                if((i + j) % 2 == 0) {
+                    screen.append("x");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(line);
+        }
+        return screen.toString();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test of building a chessboard of a given width and height.
+ * @author Viktor Kusliy (mailto:viktor.kusliy@gmail.com).
+ * @version 1.0.
+ * @since 08.10.2018.
+ */
+public class BoardTest {
+
+    @Test
+    public void whenPaintBoardWithWidthThreeAndHeightThreeThenStringWithThreeColsAndThreeRows() {
+        Board board = new Board();
+        String result = board.paint(3, 3);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("x x%s x %sx x%s", line, line, line);
+        assertThat(result, is(expected));
+
+    }
+
+    @Test
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        Board board = new Board();
+        String result = board.paint(5, 4);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("x x x%s x x %sx x x%s x x %s", line, line, line, line);
+        assertThat(result, is(expected));
+
+    }
+}
