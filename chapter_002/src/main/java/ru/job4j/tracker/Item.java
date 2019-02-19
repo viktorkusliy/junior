60e9eccc2c95c23eadaf6969c5210c638932bcc0
vkusliy <victor.kusliy@gmail.com>
2019-02-19 22:29:55 +0300

    Implement Tracker Class

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,68 @@
+package ru.job4j.tracker;
+
+public class Item {
+
+        private String name;
+        private String desc;
+        private String id;
+        private long created;
+        private String[] comments;
+
+        public Item(String id, String name, String desc, long created, String[] comments) {
+            this.id = id;
+            this.name = name;
+            this.desc = desc;
+            this.created = created;
+            this.comments = comments;
+        }
+        public Item(String name, String desc, long created) {
+            this.name = name;
+            this.desc = desc;
+            this.created = created;
+        }
+
+        public Item(String name, String desc) {
+            this.name = name;
+            this.desc = desc;
+        }
+
+        public String getId() {
+            return id;
+        }
+        public void setId(String id) {
+            this.id = id;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getDesc() {
+            return desc;
+        }
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+        public long getCreated() {
+            return created;
+        }
+        public void setCreated(long created) {
+            this.created = created;
+        }
+        public String[] getComments() {
+            return comments;
+        }
+        public void setComments(String[] comments) {
+            this.comments = comments;
+        }
+
+        @Override
+        public String toString() {
+            return "Item{" +
+                    "name='" + name + '\'' +
+                    ", desc='" + desc + '\'' +
+                    ", id='" + id + '\'' +
+                    '}';
+        }
+}
---
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,73 @@
+package ru.job4j.tracker;
+
+import java.time.LocalTime;
+import java.util.Arrays;
+
+public class Tracker {
+
+    private final Item[] items = new Item[100];
+
+    private int position;
+
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[this.position++] = item;
+        return item;
+    }
+
+    private String generateId() {
+        return new StringBuilder().append(LocalTime.now()).append(Math.random() * 1000).toString();
+    }
+
+    public boolean replace(String id, Item item) {
+        boolean result = false;
+            item.setId(id);
+            for (int i = 0; i < this.position; i++) {
+                if (this.items[i] != null && this.items[i].getId().equals(id)) {
+                    this.items[i] = item;
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public Item findById(String id) {
+            Item result = null;
+            for (int i = 0; i < this.items.length; i++) {
+                if (this.items[i].getId().equals(id)) {
+                    result = this.items[i];
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public boolean delete(String id) {
+            boolean result = false;
+            for (int i = 0; i < this.items.length; i++) {
+                if (this.items[i] != null && this.items[i].getId().equals(id)) {
+                    System.arraycopy(this.items, i + 1, this.items, i, this.items.length - i - 1);
+                    this.items[this.items.length - 1] = null;
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public Item[] findAll() {
+            return Arrays.copyOf(this.items, this.position);
+        }
+
+        public Item[] findByName(String key) {
+            Item[] found = new Item[this.position];
+            int counter = 0;
+            for (int i = 0; i < this.items.length; i++) {
+                if (this.items[i] != null && this.items[i].getName().equals(key)) {
+                    found[counter++] = this.items[i];
+                }
+            }
+            return Arrays.copyOf(found, counter);
+        }
+}
